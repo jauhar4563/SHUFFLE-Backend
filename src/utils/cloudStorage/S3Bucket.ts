b4f43295359bea0e790dd26cbb09f5fe9bada11a
jauhar4563 <jauharp02@gmail.com>
2024-06-07 21:24:48 +0530

    hello

---
+++ src/utils/cloudStorage/S3Bucket.ts
@@ -0,0 +1,29 @@
+import { PutObjectCommand, S3Client } from "@aws-sdk/client-s3";
+
+const bucketName = process.env.BUCKET_NAME;
+const region = process.env.REGION;
+const accessKeyId = process.env.AWS_ACCESS_KEY_ID;
+const secretAccessKey = process.env.AWS_SECRET_ACCESS_KEY;
+
+const s3Client = new S3Client({
+    region,
+    credentials: {
+        accessKeyId,
+        secretAccessKey
+    }
+});
+
+export const s3Upload = async (file: any) => {
+    const params:any = {
+        Bucket: bucketName,
+        Key: file.originalname,
+        Body: file.buffer,
+        ContentType: file.mimetype,
+    };
+
+    await s3Client.send(new PutObjectCommand(params));
+
+    const url = `https://${bucketName}.s3.${region}.amazonaws.com/${file.originalname}`;
+
+    return url;
+};
