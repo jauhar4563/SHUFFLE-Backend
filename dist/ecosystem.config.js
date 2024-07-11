0a39e7a23fd28a49abec698f5ae027b53ee7a8d9
Ubuntu <ubuntu@ip-172-31-38-90.ap-south-1.compute.internal>
2024-07-11 14:14:54 +0000

    Save local changes

--- dist/ecosystem.config.js
+++ dist/ecosystem.config.js
@@ -10,18 +10,24 @@ module.exports = {
         JWT_SECRET: "SHUFFLE4563",
         SESSION_SECRET: "SHUFFLESsession",
         DOMAIN_NAME: "https://shufle.online",
-        // Other environment variables
+        BUCKET_NAME: "shufle.online",
+        BUCKET_REGION: "ap-south-1",
+        ACCESS_KEY: "AKIARCMS32USRDBIQYUQ",
+        SECRET_KEY: "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"
       },
       env_production: {
         NODE_ENV: "production",
         PORT: 3000,
-        MONGO_URL:
-          "mongodb+srv://jauharp02:7510529354Jauhar@cluster0.knvvzvs.mongodb.net/SHUFFLE",
+        MONGO_URL: "mongodb+srv://jauharp02:7510529354Jauhar@cluster0.knvvzvs.mongodb.net/SHUFFLE",
         JWT_SECRET: "SHUFFLE4563",
         SESSION_SECRET: "SHUFFLESsession",
         DOMAIN_NAME: "https://shufle.online",
-        // Other environment variables specific to production
-      },
-    },
-  ],
+        BUCKET_NAME: "shufle.online",
+        BUCKET_REGION: "ap-south-1",
+        ACCESS_KEY: "AKIARCMS32USRDBIQYUQ",
+        SECRET_KEY: "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"
+      }
+    }
+  ]
 };
+
